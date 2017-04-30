28e8c7fa49656c4e6ee41cccf45a1123e4958399
Justin Horton <justinhorton3@gmail.com>
2017-04-30 17:49:05 -0500

    testing material2

--- src/main/ui/app/app.module.ts
+++ src/main/ui/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +17,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule.forRoot(),
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/main/ui/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderModule } from './header/header.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HeaderModule
+  ],
+  declarations: [],
+  exports: [HeaderModule]
+})
+export class CoreModule { }
